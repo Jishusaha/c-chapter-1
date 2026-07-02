f99cd4decc11a2cd65172d011eb585fb36645834
Jishusaha <sahajishu669@gmail.com>
2026-07-02 11:13:25 +0530

    Create bubblesort.c

---
+++ DSA/bubblesort.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,temp;
+    printf("Enter the length of the array:");
+    scanf("%d",&n);
+    int a[n];
+    printf("Enter the elements of the array:\n");
+    for(i=0;i<n;i++)
+    {
+        printf("Element %d:",i);
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n-1;i++)
+    {
+        for(j=0;j<n-i-1;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+    printf("Sorted array:\n");
+    for(i=0;i<n;i++)
+    {
+        printf("%d",a[i]);
+        if(i<n-1)
+            printf(",");
+    }
+    return 0;
+}
